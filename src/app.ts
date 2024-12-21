821cecb3a9e197ec9f6a70db51187b63be33dfb8
amateur-hacker <amateurhacker2004@gmail.com>
2024-12-21 05:47:04 +0530

    add rate limiting

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,28 @@
-import { Hono } from "hono";
+import { type Context, Hono } from "hono";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
 import { prettyJSON } from "hono/pretty-json";
 
+import { RedisStore } from "@hono-rate-limiter/redis";
+import { kv } from "@vercel/kv";
+import {
+  type Promisify,
+  type RateLimitInfo,
+  rateLimiter,
+} from "hono-rate-limiter";
 import yts from "yt-search";
 
 import { errorHandler, notFound } from "@/middlewares";
 
-const app = new Hono({ strict: false }).basePath("/api/v1");
+const app = new Hono<{
+  Variables: {
+    rateLimit: RateLimitInfo;
+    rateLimitStore: {
+      get?: (key: string) => Promisify<RateLimitInfo | undefined>;
+      resetKey: (key: string) => Promisify<void>;
+    };
+  };
+}>({ strict: false }).basePath("/api/v1");
 
 // Initialize middlewares
 app.use("*", logger(), prettyJSON());
@@ -20,6 +35,33 @@ app.use(
     allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
 );
+
+// Rate Limit
+const rateLimit = rateLimiter({
+  windowMs: 60_000,
+  limit: 10,
+  keyGenerator: (c) => c.req.header("x-forwarded-for") ?? "",
+  store: new RedisStore<{
+    Variables: {
+      rateLimit: RateLimitInfo;
+    };
+  }>({
+    client: kv,
+  }),
+  handler: (c) => {
+    const rateLimitInfo = c.get("rateLimit");
+    return c.json(
+      {
+        // message: "Rate limit exceeded. Please try again later.",
+        message: "Too many requests. Please try again later.",
+        success: false,
+        statusCode: 429,
+        rateLimit: rateLimitInfo,
+      },
+      429,
+    );
+  },
+});
 
 // Error Handler
 app.onError((_err, c) => {
@@ -34,12 +76,17 @@ app.notFound((c) => {
 });
 
 // Routes
-app.get("/", (c) => {
+app.get("/", rateLimit, (c) => {
+  const rateLimitInfo = c.get("rateLimit");
+  console.log(rateLimitInfo);
   return c.text("Hello it's a yt-search API.");
 });
 
-app.get("/yts", async (c) => {
+app.get("/yts", rateLimit, async (c) => {
   const query = c.req.query("q");
+  const rateLimitInfo = c.get("rateLimit");
+
+  console.log(rateLimitInfo);
 
   if (!query) {
     return c.json({
@@ -62,10 +109,6 @@ app.get("/yts", async (c) => {
   }));
 
   return c.json(filteredResults);
-});
-
-app.get("/hello", (c) => {
-  return c.json({ message: "Hello World!" });
 });
 
 export { app };
